3c3c4402d173b8e286a5861394fc21d1eaf52445
Anhelina <fadeevalina20@gmail.com>
2024-10-23 16:06:31 +0300

    add

---
+++ src/generics/5.ts
@@ -0,0 +1,17 @@
+export enum UserRole {
+  admin = 'admin',
+  editor = 'editor',
+  guest = 'guest',
+}
+
+type UserRoleStatuses = Record<UserRole, string>;
+
+const RoleDescription: UserRoleStatuses = {
+  [UserRole.admin]: "Admin User",
+  [UserRole.editor]: "Editor User",
+  [UserRole.guest]: "Guest User",
+};
+
+console.log(RoleDescription);
+
+export {};
